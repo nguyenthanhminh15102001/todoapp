745970f350e88139bd96884079221644c9934c66
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2023-09-29 18:14:31 +0700

    test

--- todolist_frontend/src/app/page.js
+++ todolist_frontend/src/app/page.js
@@ -65,7 +65,7 @@ export default function Home() {
                       <MenuItem value="">
                         <em>None</em>
                       </MenuItem>
-                      <MenuItem value={10}>Ten</MenuItem>
+                      <MenuItem value={10}>Test</MenuItem>
                       <MenuItem value={20}>Twenty</MenuItem>
                       <MenuItem value={30}>Thirty</MenuItem>
                     </Select>
